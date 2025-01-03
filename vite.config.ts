9b7706ac544cbd8b7c48d1f12f3eb947efc71b4a
Hernán Espinoza <hernan.espinoza.dev@gmail.com>
2025-01-03 18:04:45 -0300

    para subir a github pages g

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cabanascorihuapi/', // Base configurada para GitHub Pages
+  base: "https://hernanespinozadev.github.io/cabanascorihuapi/", // Base configurada para GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
